3da65561eda5d8c33b0e6d2553790f7541ae0da0
mrpayne207 <54715609+mrpayne207@users.noreply.github.com>
2022-10-10 12:56:20 -0500

    Update __init__.py

--- lunch/__init__.py
+++ lunch/__init__.py
@@ -4,55 +4,62 @@ from re import escape
 
 @check50.check()
 def exists():
-    """meal.py exists"""
-    check50.exists("meal.py")
+    """lunch.py exists"""
+    check50.exists("lunch.py")
 
 
 @check50.check(exists)
-def test_breakfast():
-    """input of 7:00 yields output of \"breakfast time\""""
-    input = "7:00"
-    output = "breakfast time"
-    check50.run("python3 meal.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
+def test_alunch():
+    """input of 11:44 yields output of \"A Lunch\""""
+    input = "11:26"
+    output = "A Lunch"
+    check50.run("python3 lunch.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
 
 
 @check50.check(exists)
-def test_breakfast2():
-    """input of 7:30 yields output of \"breakfast time\""""
-    input = "7:30"
-    output = "breakfast time"
-    check50.run("python3 meal.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
+def test_alunch2():
+    """input of 12:06 yields output of \"A Lunch\""""
+    input = "12:06"
+    output = "A Lunch"
+    check50.run("python3 lunch.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
 
 
 @check50.check(exists)
-def test_lunch():
-    """input of 12:42 yields output of \"lunch time\""""
-    input = "12:42"
-    output = "lunch time"
-    check50.run("python3 meal.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
+def test_blunch():
+    """input of 12:07 yields output of \"B Lunch\""""
+    input = "12:07"
+    output = "B Lunch"
+    check50.run("python3 lunch.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
 
 
 @check50.check(exists)
-def test_lunch():
-    """input of 13:00 yields output of \"lunch time\""""
-    input = "13:00"
-    output = "lunch time"
-    check50.run("python3 meal.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
+def test_blunch2():
+    """input of 12:46 yields output of \"B Lunch\""""
+    input = "12:46"
+    output = "B Lunch"
+    check50.run("python3 lunch.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
 
 
 @check50.check(exists)
-def test_dinner():
-    """input of 18:32 yields output of \"dinner time\""""
-    input = "18:32"
-    output = "dinner time"
-    check50.run("python3 meal.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
+def test_clunch():
+    """input of 12:47 yields output of \"C Lunch\""""
+    input = "12:47"
+    output = "C Lunch"
+    check50.run("python3 lunch.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
+    
+@check50.check(exists)
+def test_clunch2():
+    """input of 13:26 yields output of \"C Lunch\""""
+    input = "13:26"
+    output = "C Lunch"
+    check50.run("python3 lunch.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
 
 
 @check50.check(exists)
 def test_no_output():
-    """input of 11:11 yields no output"""
-    input = "11:11"
-    output = ""
+    """input of 11:00 yields output of \"No lunch at this time\""""
+    input = "11:00"
+    output = "No lunch at this time"
     check50.run("python3 meal.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
 
 

5027818f176d13add1a9c8f989a96e6c7bb3014e
mrpayne207 <54715609+mrpayne207@users.noreply.github.com>
2022-10-10 09:47:20 -0500

    Update __init__.py

--- hello/__init__.py
+++ hello/__init__.py
@@ -9,9 +9,13 @@ def exists():
 @check50.check(exists)
 def veronica():
     """responds to name Veronica."""
-    check50.run("python3 hello.py").stdin("Veronica", prompt=True).stdout("hello, Veronica").exit()
+    input = "Veronica"
+    output = "hello, Veronica"
+    check50.run("python3 hello.py").stdin(input, prompt=True).stdout(regex(output), output, regex = True).exit()
 
 @check50.check(exists)
 def david():
     """responds to name David"""
-    check50.run("python3 hello.py").stdin("David", prompt=True).stdout("hello, David").exit()
+    input = "David"
+    output = "hello, David"
+    check50.run("python3 hello.py").stdin(input, prompt=True).stdout(regex(output), output, regex = True).exit()

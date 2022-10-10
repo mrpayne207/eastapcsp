948d96d7d454aaa2066b06ff95e942045295423d
mrpayne207 <54715609+mrpayne207@users.noreply.github.com>
2022-10-10 08:20:25 -0500

    Update __init__.py

--- unit1/hellolang/__init__.py
+++ unit1/hellolang/__init__.py
@@ -1,12 +1,20 @@
-import check50
 import re
 
-@check50.check()
-def exists():
-    """hellolang.py exists"""
-    check50.exists("hellolang.py")
+from check50 import *
 
-@check50.check(exists)
-def compiles():
-    """hellolang.py compiles"""
-    check50.run("python3 hellolang.py").stdin("foo").exit(0)
+class Hello(Checks):
+    
+    @check()
+    def exists(self):
+        """helloLang.py exists."""
+        self.require("helloLang.py")
+    
+    @check("exists")
+    def rob(self):
+        """responds to name Rob."""
+        self.spawn("python helloLang.py").stdin("Rob").stdout("hello, Rob\n")
+    
+    @check("exists")
+    def layla(self):
+        """responds to name Layla."""
+        self.spawn("python helloLang.py").stdin("Layla").stdout("hello, Layla\n")

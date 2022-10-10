fdcecd35a56a69352e9a976ec8c1e31484f4c4fc
mrpayne207 <54715609+mrpayne207@users.noreply.github.com>
2022-10-10 13:59:43 -0500

    Update __init__.py

--- rps1/__init__.py
+++ rps1/__init__.py
@@ -23,6 +23,20 @@ def test_p1wins():
     output = "PLAYER 1 WINS"
     check50.run("python3 rps1.py").stdin(input[0], prompt=True).stdin(input[1], prompt=True).stdout(regex(output), output, regex=True).exit()
 
+@check50.check(exists)
+def test_tie():
+    """input of \"p\" and \"p\" results in \"PLAYER 1 WINS\""""
+    input = ["p", "p"]
+    output = "TIE"
+    check50.run("python3 rps1.py").stdin(input[0], prompt=True).stdin(input[1], prompt=True).stdout(regex(output), output, regex=True).exit()
+    
+@check50.check(exists)
+def test_invalid():
+    """input of \"f\" and \"b\" results in \"PLAYER 1 WINS\""""
+    input = ["f", "b"]
+    output = "Make sure to enter r for rock, p for paper or s for scissors"
+    check50.run("python3 rps1.py").stdin(input[0], prompt=True).stdin(input[1], prompt=True).stdout(regex(output), output, regex=True).exit()
+    
 def regex(play):
     """match case-insensitively with only whitespace on either side"""
     return fr'(?i)^\s*{escape(play)}\s*$'

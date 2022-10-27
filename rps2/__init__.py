de846cfcf3fee387dc4b5bf4a2088bbc359d03c2
mrpayne207 <54715609+mrpayne207@users.noreply.github.com>
2022-10-26 20:47:26 -0500

    Update __init__.py

--- rps2/__init__.py
+++ rps2/__init__.py
@@ -1,1 +1,16 @@
-asdf
+import check50
+from re import escape
+
+
+@check50.check()
+def exists():
+    """rps2.py exists"""
+    check50.exists("rps2.py")
+
+
+@check50.check(exists)
+def test_p1isR():
+    """input of \"R\" for Player 1 matches \"TIE\" for \"Computer = r\", \"COMPUTER WINS\" for \"Computer = p\" or \"PLAYER 1 WINS\" for \"Computer = s\""""
+    output = check50.run("python3 rps2.py").stdout()
+    if not any(answer in output for answer in ["Computer = r\nTIE", "Computer = p\nCOMPUTER WINS", "Computer = s\nPLAYER 1 WINS"]):
+      raise check50.Failure("no match found")

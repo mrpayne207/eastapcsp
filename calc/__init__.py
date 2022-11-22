a759ba3e7144fbde8adb3436ae5d77601701a32d
mrpayne207 <54715609+mrpayne207@users.noreply.github.com>
2022-11-22 14:09:13 -0600

    Update __init__.py

--- calc/__init__.py
+++ calc/__init__.py
@@ -9,4 +9,4 @@ def exists():
 @check50.check(exists)
 def test():
     """input of x = 4.32 and y = 8.67 yields correct results"""
-    check50.run("python3 calc.py").stdin("4.32", prompt=True).stdin("8.67", prompt=True).stdout('x + y = 12.99\nx - y -4.35\nx * y = 37.45\nx / y = 0.50\nx mod y = 4.32\n').exit()
+    check50.run("python3 calc.py").stdin("4.32", prompt=True).stdin("8.67", prompt=True).stdout('x + y = 12.99\nx - y = -4.35\nx * y = 37.45\nx / y = 0.50\nx mod y = 4.32\n').exit()

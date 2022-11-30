60a07981d0b87b664d57e1a7f7ad0b2313b2f0c9
mrpayne207 <54715609+mrpayne207@users.noreply.github.com>
2022-11-30 12:23:14 -0600

    Update __init__.py

--- dice/__init__.py
+++ dice/__init__.py
@@ -6,3 +6,19 @@ from re import escape
 def exists():
     """dice.py exists"""
     check50.exists("dice.py")
+
+@check50.check(exists)
+def test_string_level():
+    """dice.py rejects non-numeric level"""
+    check50.run("python3 dice.py").stdin("cat", prompt=True).reject()
+
+
+@check50.check(exists)
+def test_integer_level():
+    """dice.py rejects out-of-range level"""
+    check50.run("python3 dice.py").stdin("0", prompt=True).reject()
+    
+@check50.check(exists)
+def test_valid_level():
+    """dice.py accepts valid level"""
+    check50.run("python3 dice.py").stdin("2", prompt=True).kill()

230bdd6c57842ce0146bed7c9f4687dcb7ac8c67
mrpayne207 <54715609+mrpayne207@users.noreply.github.com>
2022-12-02 09:44:45 -0600

    Update __init__.py

--- wordcheck/__init__.py
+++ wordcheck/__init__.py
@@ -14,8 +14,33 @@ def testNumber():
 
 @check50.check(exists)
 def testSymbol():
-    """word_checker.py rejects non-letter input"""
+    """word_checker.py rejects non-letter word input"""
     check50.run("python3 word_checker.py").stdin("hello!", prompt=True).reject()
+    
+@check50.check(exists)
+def testShort():
+    """word_checker.py rejects short word input"""
+    check50.run("python3 word_checker.py").stdin("no", prompt=True).reject()
+
+@check50.check(exists)
+def testLong():
+    """word_checker.py rejects long word input"""
+    check50.run("python3 word_checker.py").stdin("toolong", prompt=True).reject()
+    
+@check50.check(exists)
+def testNumGuess():
+    """word_checker.py rejects number guess input"""
+    check50.run("python3 word_checker.py").stdin("hello", prompt=True).stdin("greeting", prompt=True).stdin("1", prompt=True).reject()
+
+@check50.check(exists)
+def testSymbolGuess():
+    """word_checker.py rejects non-letter guess input"""
+    check50.run("python3 word_checker.py").stdin("hello", prompt=True).stdin("greeting", prompt=True).stdin("$", prompt=True).reject()
+
+@check50.check(exists)
+def testDoubleGuess():
+    """word_checker.py rejects non-single letter guess input"""
+    check50.run("python3 word_checker.py").stdin("hello", prompt=True).stdin("greeting", prompt=True).stdin("ab", prompt=True).reject()
     
 def regex(text):
     """match case-sensitively, allowing for characters on either side"""

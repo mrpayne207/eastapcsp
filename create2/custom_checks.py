8b5753f90ff33bbec9f1f7888035b036e393f628
mrpayne207 <54715609+mrpayne207@users.noreply.github.com>
2022-12-06 09:24:44 -0600

    Create custom_checks.py

---
+++ create2/custom_checks.py
@@ -0,0 +1,35 @@
+import sys
+from inspect import getmembers, isfunction
+
+match sys.argv[1]:
+
+    case "main_function":
+        with open("create3.py") as file:
+
+            # Look for main function definition and avoid importing any libraries
+            assert "def main" in "".join(file.read().splitlines())
+
+    case "custom_functions":
+        with open("create3.py") as file:
+            function_counter = 0
+            condition_counter = 0
+            loop_counter = 0
+            for line in file.readlines():
+                if "def main" in line.strip():
+                    continue
+                elif "def" in line.strip():
+                    function_counter += 1
+                if ("if" or "elif" or "else") in line.strip():
+                    condition_counter += 1
+                if ("for" or "while") in line.strip():
+                    loop_counter += 1
+                  
+            
+            # Ensure there are at least 1 top-level functions other than main
+            assert function_counter >= 1
+            
+            # Ensure there are at least 1 top-level functions other than main
+            assert condition_counter >= 1
+            
+            # Ensure there are at least 1 top-level functions other than main
+            assert loop_counter >= 1

09429846fc517fe3ffd474844c2eb69d7ece2b56
mrpayne207 <54715609+mrpayne207@users.noreply.github.com>
2023-01-13 12:57:55 -0600

    Update __init__.py

--- selection/__init__.py
+++ selection/__init__.py
@@ -25,13 +25,13 @@ def testFloat():
 @check50.check(exists)
 def test_list():
     """input of [1, 3, 8, 0, -5, -1] sorts correction"""
-    input1 = 6
-    input2 = 1
-    input3 = 3
-    input4 = 8
-    input5 = 0
-    input6 = -5
-    input7 = -1
+    input1 = "6"
+    input2 = "1"
+    input3 = "3"
+    input4 = "8"
+    input5 = "0"
+    input6 = "-5"
+    input7 = "-1"
     output = "Round 0: [1, 3, 8, 0, -5, -1]\nRound 1: [-5, 3, 8, 0, 1, -1]\nRound 2: [-5, -1, 8, 0, 1, 3]\nRound 3: [-5, -1, 0, 8, 1, 3]\nRound 4: [-5, -1, 0, 1, 8, 3]\nRound 5: [-5, -1, 0, 1, 3, 8]"
     check50.run("python3 selection_sort.py").stdin(input1, prompt=True).stdin(input2, prompt=True).stdin(input3, prompt=True).stdin(input4, prompt=True).stdin(input5, prompt=True).stdin(input6, prompt=True).stdin(input7, prompt=True).stdout(regex(output), output, regex=True).exit()
 

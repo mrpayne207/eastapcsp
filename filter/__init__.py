16eb0c4762b83bc5169451268ffbe04395f2529f
mrpayne207 <54715609+mrpayne207@users.noreply.github.com>
2023-02-08 14:35:58 -0600

    Update __init__.py

--- filter/__init__.py
+++ filter/__init__.py
@@ -1,10 +1,11 @@
 import check50
 
 HASHES = {
-    "muppet_01.jpg" : 'b782475a5dd3b0d7b3202cebc8a70f5d795dd196e9c18564691d5edc11ccf7c9',
-    "muppet_02.jpg" : 'f989a97f95563f587e158bb55a1fc6dba075f1e221acec988612caaa0d1a6b78',
-    "muppet_03.jpg" : 'fa7b30def84d46559c54e718d167de93e52785f9b613db8a647ddcfbbe9aff98',
-    "muppet_04.jpg" : '8f59304412e181f1a18d3b36ad88d7b2911a7eea8f471c7e437e2cbed5893152',
+    "MEHS.jpg" : '963f6887460addafb99d302578f3c10ef688e924b6c8e9abebdda9a9c0df45fe'
+    "black_white.png" : '56751daacaa452a8f11a35f5238aa6d896d9c37e993a32e10294add9b2a95abf'
+    "crop.png" : '74061251f0ca818c9aa7f8b86e7bbb1efaa4031eb6422b4f755fc67285f6f1fd'
+    "reflect.png" : 'ff0a8cb53a28ac9eab1ad02e92530af1999d8a14da62ad12770522b64962bbd1'
+    "blur.png" : '7831d7d8a3730adb34222c65d9379db362729e5868d703fc2fd61e41f6a806bc'
 }
 
 
@@ -24,70 +25,48 @@ def test_fewer_arguments():
 
 @check50.check(exists)
 def test_non_existent_file():
-    """shirt.py exits given a non-existent file"""
-    exit = check50.run("python3 shirt.py non_existent_file.jpg").exit()
+    """filter.py exits given a non-existent file"""
+    exit = check50.run("python3 filter.py non_existent_file.jpg").exit()
     if exit == 0:
         raise check50.Failure(f"Expected non-zero exit code.")
-
-
-@check50.check(exists)
-def test_mismatched_extension():
-    """shirt.py exits given an output file with a different extension than input file"""
-    check50.include("muppet_01.jpg")
-    exit = check50.run("python3 shirt.py muppet_01.jpg muppet_01_out.png").exit()
-    if exit == 0:
-        raise check50.Failure(f"Expected non-zero exit code.")
-
 
 @check50.check(exists)
 def test_more_arguments():
-    """shirt.py exits given more than two command-line arguments"""
-    for file in ["muppet_01.jpg", "muppet_02.jpg", "muppet_03.jpg"]:
+    """filter.py exits given more than two command-line arguments"""
+    for file in ["MEHS.jpg", "MEHS1.jpg", "MEHS2.jpg"]:
         check50.include(file)
-    exit = check50.run("python3 lines.py muppet_01.jpg muppet_02.jpg muppet_03.jpg").exit()
+    exit = check50.run("python3 filter.py MEHS.jpg MEHS1.jpg MEHS2.jpg").exit()
     if exit == 0:
         raise check50.Failure(f"Expected non-zero exit code.")
 
 
 @check50.check(exists)
-def test_muppet_01():
-    """shirt.py correctly displays shirt on muppet_01.jpg"""
-    test_shirt("muppet_01.jpg")
+def test_black_white():
+    """filter.py correctly filters black and white on MEHS.jpg"""
+    test_shirt("black_white.png")
 
 
 @check50.check(exists)
-def test_muppet_02():
-    """shirt.py correctly displays shirt on muppet_02.jpg"""
-    test_shirt("muppet_02.jpg")
+def test_crop():
+    """filter.py correctly filters crop on MEHS.jpg"""
+    test_shirt("crop.png")
 
 
 @check50.check(exists)
-def test_muppet_03():
-    """shirt.py correctly displays shirt on muppet_03.jpg"""
-    test_shirt("muppet_03.jpg")
+def test_reflect():
+    """filter.py correctly filters reflect on MEHS.jpg"""
+    test_shirt("reflect.png")
 
 
 @check50.check(exists)
-def test_muppet_04():
-    """shirt.py correctly displays shirt on muppet_04.jpg"""
-    test_shirt("muppet_04.jpg")
-
-
-@check50.check(exists)
-def test_muppet_05():
-    """shirt.py correctly displays shirt on muppet_05.jpg"""
-    test_shirt("muppet_05.jpg")
-
-
-@check50.check(exists)
-def test_muppet_06():
-    """shirt.py correctly displays shirt on muppet_06.jpg"""
-    test_shirt("muppet_06.jpg")
+def test_blur():
+    """filter.py correctly filters blur on MEHS.jpg"""
+    test_shirt("blur.jpg")
 
 
 def test_shirt(photo):
     check50.include(photo)
-    check50.run(f"python3 shirt.py {photo} {photo[:-4]}_out.jpg").exit(0)
-    hash = check50.hash(f"{photo[:-4]}_out.jpg")
+    check50.run(f"python3 filter.py MEHS.jpg {photo[:-4]}.png").exit(0)
+    hash = check50.hash(f"{photo[:-4]}.png")
     if hash != HASHES[photo]:
         raise check50.Failure("Image does not match")

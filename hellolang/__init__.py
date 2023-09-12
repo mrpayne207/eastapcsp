e4ca67ef32a986bb30d62420b6f280ec39800755
mrpayne207 <54715609+mrpayne207@users.noreply.github.com>
2023-09-11 21:16:46 -0500

    Update __init__.py

--- hellolang/__init__.py
+++ hellolang/__init__.py
@@ -3,14 +3,14 @@ import check50
 @check50.check()
 def exists():
     """hellolang.py exists."""
-    check50.exists("hellolang.py")
+    check50.exists("hello_lang.py")
 
 @check50.check(exists)
 def veronica():
     """responds to name Emma."""
-    check50.run("python3 hellolang.py").stdin("Emma").stdout("Emma").exit()
+    check50.run("python3 hello_lang.py").stdin("Emma").stdout("Emma").exit()
 
 @check50.check(exists)
 def brian():
     """responds to name Rodrigo."""
-    check50.run("python3 hellolang.py").stdin("Rodrigo").stdout("Rodrigo").exit()
+    check50.run("python3 hello_lang.py").stdin("Rodrigo").stdout("Rodrigo").exit()

5cd0799adf5dee3a8eb4af40cccf693b04c2f675
mrpayne207 <54715609+mrpayne207@users.noreply.github.com>
2023-09-11 21:19:00 -0500

    Update __init__.py

--- calc/__init__.py
+++ calc/__init__.py
@@ -12,7 +12,7 @@ def test():
     """input of x = 4.32 and y = 8.67 yields correct results"""
     input1 = "4.32"
     input2 = "8.67"
-    output = "x + y = 12.99\nx - y = -4.35\nx * y = 37.45\nx / y = 0.50\nx mod y = 4.32"
+    output = "x + y = 12.99\nx - y = -4.35\nx * y = 37.45\nx / y = 0.50"
     check50.run("python3 calc.py").stdin(input1, prompt=True).stdin(input2, prompt=True).stdout(regex(output), output, regex=True).exit()
     
 def regex(text):

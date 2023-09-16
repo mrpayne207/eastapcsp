3870971b5fb5a739b858395a13b289043f24e0ef
mrpayne207 <54715609+mrpayne207@users.noreply.github.com>
2023-09-16 13:32:51 -0500

    Update __init__.py

--- convert/__init__.py
+++ convert/__init__.py
@@ -1,1 +1,19 @@
+import check50
+from re import escape
 
+
+@check50.check()
+def exists():
+    """convert.py exists"""
+    check50.exists("convert.py")
+
+@check50.check(exists)
+def test_100():
+    """input of 100 yields correct results"""
+    input1 = "100"
+    output = "37.78"
+    check50.run("python3 convert.py").stdin(input1, prompt=True).stdout(regex(output), output, regex=True).exit()
+    
+def regex(text):
+    """match case-sensitively, allowing for characters on either side"""
+    return fr'^.*{escape(text)}.*$'

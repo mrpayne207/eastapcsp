3b92ad250776dad13f42ce2628213f7a76da50d8
mrpayne207 <54715609+mrpayne207@users.noreply.github.com>
2023-09-16 13:39:31 -0500

    Update __init__.py

--- convert/__init__.py
+++ convert/__init__.py
@@ -20,7 +20,20 @@ def test_50():
     input1 = "50"
     output = "10.0"
     check50.run("python3 convert.py").stdin(input1, prompt=True).stdout(regex(output), output, regex=True).exit()
-    
+
+@check50.check(exists)
+def main_function():
+    """main function exists"""
+    check50.include("custom_checks.py")
+    check50.run("python3 custom_checks.py main_function").exit(0)
+
+
+@check50.check(exists)
+def custom_function():
+    """implemented at least 1 top-level function other than main"""
+    check50.include("custom_checks.py")
+    check50.run("python3 custom_checks.py custom_functions").exit(0)
+
 def regex(text):
     """match case-sensitively, allowing for characters on either side"""
     return fr'^.*{escape(text)}.*$'

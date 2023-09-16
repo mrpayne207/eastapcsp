4c7a8d1bc38741a7e6a94e314a2d6100d13288f9
mrpayne207 <54715609+mrpayne207@users.noreply.github.com>
2023-09-16 13:35:42 -0500

    Update __init__.py

--- convert/__init__.py
+++ convert/__init__.py
@@ -9,9 +9,16 @@ def exists():
 
 @check50.check(exists)
 def test_100():
-    """input of 100 yields correct results"""
+    """input of 100 deg F yields 37.78 deg C"""
     input1 = "100"
-    output = "37.78"
+    output = "37.8"
+    check50.run("python3 convert.py").stdin(input1, prompt=True).stdout(regex(output), output, regex=True).exit()
+
+@check50.check(exists)
+def test_50():
+    """input of 50 deg F yields 10.00 deg C"""
+    input1 = "50"
+    output = "10.0"
     check50.run("python3 convert.py").stdin(input1, prompt=True).stdout(regex(output), output, regex=True).exit()
     
 def regex(text):

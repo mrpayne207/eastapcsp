d7574dbfad071f2e03c4dea4bb186a70ec3df1c3
mrpayne207 <54715609+mrpayne207@users.noreply.github.com>
2023-09-27 13:18:52 -0500

    Update __init__.py

--- change/__init__.py
+++ change/__init__.py
@@ -12,14 +12,14 @@ def test41():
     """input of .41 has correct results"""
     input1 = ".41"
     output = "1 [Qq]uarter(s)\n1 [Dd]ime(s)\n1 [Nn]ickle(s)\n1 [Pp]ennie(s)"
-    check50.run("python3 calc.py").stdin(input1, prompt=True).stdout(regex(output), output, regex=True).exit()
+    check50.run("python3 change.py").stdin(input1, prompt=True).stdout(regex(output), output, regex=True).exit()
 
 @check50.check(exists)
 def test93():
     """input of .93 has correct results"""
     input1 = ".93"
     output = "3 [Qq]uarter(s)\n1 [Dd]ime(s)\n1 [Nn]ickle(s)\n3 [Pp]ennie(s)"
-    check50.run("python3 calc.py").stdin(input1, prompt=True).stdout(regex(output), output, regex=True).exit()
+    check50.run("python3 change.py").stdin(input1, prompt=True).stdout(regex(output), output, regex=True).exit()
 
 @check50.check(exists)
 def main_function():

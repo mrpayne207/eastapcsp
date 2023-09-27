6670e5193e081c2c343056335f654477e25bc500
mrpayne207 <54715609+mrpayne207@users.noreply.github.com>
2023-09-27 10:39:25 -0500

    Update __init__.py

--- create1/__init__.py
+++ create1/__init__.py
@@ -1,1 +1,24 @@
+import check50
+from re import escape
 
+@check50.check()
+def exists():
+    """create1.py exists"""
+    check50.exists("create1.py")
+
+@check50.check(exists)
+def main_function():
+    """main function exists"""
+    check50.include("custom_checks.py")
+    check50.run("python3 custom_checks.py main_function").exit(0)
+
+
+@check50.check(exists)
+def custom_function():
+    """implemented at least 1 top-level function other than main"""
+    check50.include("custom_checks.py")
+    check50.run("python3 custom_checks.py custom_functions").exit(0)
+
+def regex(text):
+    """match case-sensitively, allowing for characters on either side"""
+    return fr'^.*{escape(text)}.*$'

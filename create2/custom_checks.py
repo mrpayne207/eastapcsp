c3023476a6cf60169d3e94904b86c5bf1a6bf657
mrpayne207 <54715609+mrpayne207@users.noreply.github.com>
2023-10-26 09:51:06 -0500

    Update custom_checks.py

--- create2/custom_checks.py
+++ create2/custom_checks.py
@@ -4,13 +4,13 @@ from inspect import getmembers, isfunction
 match sys.argv[1]:
 
     case "main_function":
-        with open("create3.py") as file:
+        with open("create2.py") as file:
 
             # Look for main function definition and avoid importing any libraries
             assert "def main" in "".join(file.read().splitlines())
 
     case "custom_functions":
-        with open("create3.py") as file:
+        with open("create2.py") as file:
             function_counter = 0
             condition_counter = 0
             loop_counter = 0
@@ -21,8 +21,6 @@ match sys.argv[1]:
                     function_counter += 1
                 if ("if" or "elif" or "else") in line.strip():
                     condition_counter += 1
-                if ("for" or "while") in line.strip():
-                    loop_counter += 1
                   
             
             # Ensure there are at least 1 top-level functions other than main
@@ -30,9 +28,6 @@ match sys.argv[1]:
             
             # Ensure there are at least 1 top-level functions other than main
             assert condition_counter >= 1
-            
-            # Ensure there are at least 1 top-level functions other than main
-            assert loop_counter >= 1
     
     case "condition":
         with open("create3.py") as file:
@@ -48,17 +43,3 @@ match sys.argv[1]:
             # Ensure there are at least 1 top-level functions other than main
             assert condition_counter >= 1
             
-    
-    case "loop":
-        with open("create3.py") as file:
-            condition_counter = 0
-            for line in file.readlines():
-                if "def main" in line.strip():
-                    continue
-                elif "def" in line.strip():
-                    continue
-                if ("for" or "while") in line.strip():
-                    loop_counter += 1
-           
-            # Ensure there is at least 1 loop in the program
-            assert loop_counter >= 1

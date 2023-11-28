1e25beb6cb5935f6d433501a651c6723c4af9c74
mrpayne207 <54715609+mrpayne207@users.noreply.github.com>
2023-11-28 15:26:10 -0600

    Create __init__.py

---
+++ dice1/__init__.py
@@ -0,0 +1,29 @@
+import check50
+from re import escape
+
+
+@check50.check()
+def exists():
+    """dice.py exists"""
+    check50.exists("dice.py")
+
+@check50.check(exists)
+def test_string_level():
+    """dice.py rejects non-numeric dice"""
+    check50.run("python3 dice.py").stdin("cat", prompt=True).reject()
+
+@check50.check(exists)
+def test_decimal_level():
+    """dice.py rejects decimal dice"""
+    check50.run("python3 dice.py").stdin("0.1", prompt=True).reject()
+    
+
+@check50.check(exists)
+def test_integer_level():
+    """dice.py rejects out-of-range dice"""
+    check50.run("python3 dice.py").stdin("0", prompt=True).reject()
+    
+@check50.check(exists)
+def test_valid_level():
+    """dice.py accepts valid dice"""
+    check50.run("python3 dice.py").stdin("2", prompt=True).kill()

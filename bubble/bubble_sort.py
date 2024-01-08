078651448ac75206384a7b5b4cfffb3e3c2db057
mrpayne207 <54715609+mrpayne207@users.noreply.github.com>
2024-01-08 09:52:06 -0600

    Add files via upload

---
+++ bubble/bubble_sort.py
@@ -0,0 +1,43 @@
+def main():
+    # get a positive list size from the user, store in variable n
+
+
+    # create a blank list num_list
+
+
+
+    # populate num_list with n integers
+
+
+
+    # print the original list
+
+
+
+    # call the bubble_sort porcedure, pass in num_list as an argument
+
+
+
+# create a procedure that validates the input is an integer
+
+
+# create a porcedure that implements bubble_sort that takes in a list and uses the bubble sort algorithm
+
+
+    # get the length of the list, store in variable x
+
+
+
+    # traverse every element except for the last element
+
+
+        # traverse the unsoreted list each time (from 0 to x - i - 1)
+
+
+            # if the current elemement (j) is less bigger than the next element (j+1), swap them
+
+
+        # print the status of the list after each round through the list
+
+
+main()

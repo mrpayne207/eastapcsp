6c4dd229497def576f108394c5a7b9202dac13e9
mrpayne207 <54715609+mrpayne207@users.noreply.github.com>
2024-01-16 15:16:44 -0600

    Add files via upload

---
+++ grade/grade1.py
@@ -0,0 +1,46 @@
+# student name
+# grade1.py
+
+# get a number of grade, input the grades, call procedures to find the min, max and average, print the results
+def main():
+    # get a positive integer number of grades, validating it is a positive integer
+    # if it is not a positive integer, give an erroro message
+
+
+    # create a blank list to store grades in
+
+    # populate n number of integer grades from the user, validating the grades are between 40 and 100, inclusive
+    # if the grades are not between 40 and 100, inclusive, give the user an error message
+
+
+    #find the minimum value in the list of grades
+
+
+    #find the maximum value in the list of grades
+
+
+    #find the average value and letter grade from the list of grades
+
+
+    # print results
+
+
+# create a procedure to validate integer input
+#def get_int(prompt, error_message):
+
+# create a procedure to find the minimum value in a list
+#def find_min(list):
+
+# create a procedure to find the maximum value in a list
+#def find_max(list):
+
+# create a procedure to find the average value of a list and
+# correlate that average value with a letter grade based on the standard grading scale
+#def find_ave(list):
+    # calculate the average grade, rounding to the nearest tenth
+
+
+    # determine the letter grade based on the standard grading scale
+
+#call the main procedure
+main()

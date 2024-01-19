3dc37e4b2aa16984f59406acfee5dbe14f438dbf
mrpayne207 <54715609+mrpayne207@users.noreply.github.com>
2024-01-19 10:58:03 -0600

    Update __init__.py

--- grade/__init__.py
+++ grade/__init__.py
@@ -31,55 +31,17 @@ def testGrade():
     check50.run("python3 grade1.py").stdin(input1, prompt=True).stdin(input2, prompt=True).reject()
 
 @check50.check(exists)
-def testA():
-    """grades of [85, 90, 95] result in 90.0 and an A"""
-    input1 = "3"
-    input2 = "85"
-    input3 = "90"
-    input4 = "95"
-    output = "Grade List: [85, 90, 95]\nMinimum Grade: 85\nMaximum Grade: 95\nAverage Grade: 90.0, Letter Grade: A"
-    check50.run("python3 grade1.py").stdin(input1, prompt=True).stdin(input2, prompt=True).stdin(input3, prompt=True).stdin(input4, prompt=True).stdout(regex(output), output, regex=True).exit()
+def testRetake():
+    """grades of [73, 78, 86, 61, 100] produce correct result"""
+    input1 = "5"
+    input2 = "73"
+    input3 = "78"
+    input4 = "86"
+    input5 = "61"
+    input6 = "100"
+    output = "Average Grade: 79.6\nYou should retake Test 1, your score was: 73\nYou should retake Test 2, your score was: 78\nYou should retake Test 4, your score was: 613"
+    check50.run("python3 grade1.py").stdin(input1, prompt=True).stdin(input2, prompt=True).stdin(input3, prompt=True).stdin(input4, prompt=True).stdin(input5, prompt=True).stdin(input6, prompt=True).stdout(regex(output), output, regex=True).exit()
 
-@check50.check(exists)
-def testB():
-    """grades of [88, 89, 91] result in 89.3 and an B"""
-    input1 = "3"
-    input2 = "88"
-    input3 = "89"
-    input4 = "91"
-    output = "Grade List: [88, 89, 91]\nMinimum Grade: 88\nMaximum Grade: 91\nAverage Grade: 89.3, Letter Grade: B"
-    check50.run("python3 grade1.py").stdin(input1, prompt=True).stdin(input2, prompt=True).stdin(input3, prompt=True).stdin(input4, prompt=True).stdout(regex(output), output, regex=True).exit()
-
-@check50.check(exists)
-def testC():
-    """grades of [70, 75] result in 72.5 and an C"""
-    input1 = "2"
-    input2 = "70"
-    input3 = "75"
-    output = "Grade List: [70, 75]\nMinimum Grade: 70\nMaximum Grade: 75\nAverage Grade: 72.5, Letter Grade: C"
-    check50.run("python3 grade1.py").stdin(input1, prompt=True).stdin(input2, prompt=True).stdin(input3, prompt=True).stdout(regex(output), output, regex=True).exit()
-
-@check50.check(exists)
-def testD():
-    """grades of [68, 69, 72] result in 69.7 and an D"""
-    input1 = "3"
-    input2 = "68"
-    input3 = "69"
-    input4 = "72"
-    output = "Grade List: [68, 69, 72]\nMinimum Grade: 68\nMaximum Grade: 72\nAverage Grade: 69.7, Letter Grade: D"
-    check50.run("python3 grade1.py").stdin(input1, prompt=True).stdin(input2, prompt=True).stdin(input3, prompt=True).stdin(input4, prompt=True).stdout(regex(output), output, regex=True).exit()
-
-@check50.check(exists)
-def testF():
-    """grades of [0, 50, 100] result in 50.0 and an F"""
-    input1 = "3"
-    input2 = "40"
-    input3 = "50"
-    input4 = "60"
-    output = "Grade List: [40, 50, 60]\nMinimum Grade: 40\nMaximum Grade: 60\nAverage Grade: 50.0, Letter Grade: F"
-    check50.run("python3 grade1.py").stdin(input1, prompt=True).stdin(input2, prompt=True).stdin(input3, prompt=True).stdin(input4, prompt=True).stdout(regex(output), output, regex=True).exit()
-
-    
 def regex(text):
     """match case-sensitively, allowing for characters on either side"""
     return fr'^.*{escape(text)}.*$'

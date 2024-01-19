be868f658bd215384a2144257fb319e2f63ee2db
mrpayne207 <54715609+mrpayne207@users.noreply.github.com>
2024-01-19 10:42:13 -0600

    Add files via upload

---
+++ grade/grade.py
@@ -0,0 +1,52 @@
+# student name
+# grade.py
+
+# get a number of grade, input the grades, call procedures to find the min, max and average, print the results
+def main():
+    # get a positive integer number of grades, validating it is a positive integer
+    # if it is not a positive integer, give an erroro message
+
+
+    # create a blank list to store grades in
+
+
+    # populate n number of integer grades from the user, validating the grades are between 40 and 100, inclusive
+    # if the grades are not between 40 and 100, inclusive, give the user an error message
+
+
+    #find the minimum value in the list of grades
+
+
+    #find the maximum value in the list of grades
+
+
+    #find the average value and letter grade from the list of grades
+
+
+    # call retake procedure, passing in grade_list and average score
+
+
+# create a procedure to validate integer input
+#def get_int(prompt, error_message):
+
+
+# create a procedure to find the minimum value in a list
+#def find_min(grade_list):
+
+
+# create a procedure to find the maximum value in a list
+#def find_max(grade_list):
+
+
+# create a procedure to find the average value of a list and
+# correlate that average value with a letter grade based on the standard grading scale
+#def find_ave(grade_list):
+    # calculate the average grade, rounding to the nearest tenth
+
+
+# identify the tests that should be retaken
+#def find_retake(grade_list, ave):
+    
+
+#call the main procedure
+main()

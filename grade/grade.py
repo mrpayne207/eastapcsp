5e5ef17738a28db294afde0836653d744f08c655
mrpayne207 <54715609+mrpayne207@users.noreply.github.com>
2024-01-19 11:06:56 -0600

    Update grade.py

--- grade/grade.py
+++ grade/grade.py
@@ -5,16 +5,13 @@
 def main():
     # get a positive integer number of grades, validating it is a positive integer
     # if it is not a positive integer, give an erroro message
-
+    
 
     # create a blank list to store grades in
-
 
     # populate n number of integer grades from the user, validating the grades are between 40 and 100, inclusive
     # if the grades are not between 40 and 100, inclusive, give the user an error message
 
-
-    #find the minimum value in the list of grades
 
 
     #find the maximum value in the list of grades
@@ -41,12 +38,11 @@ def main():
 # create a procedure to find the average value of a list and
 # correlate that average value with a letter grade based on the standard grading scale
 #def find_ave(grade_list):
-    # calculate the average grade, rounding to the nearest tenth
 
 
 # identify the tests that should be retaken
 #def find_retake(grade_list, ave):
-    
+
 
 #call the main procedure
 main()

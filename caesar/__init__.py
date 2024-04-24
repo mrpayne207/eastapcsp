61f654d83c8e2b663f64075aca39a9bd99d4f3bf
mrpayne207 <54715609+mrpayne207@users.noreply.github.com>
2024-04-24 13:35:34 -0500

    Update __init__.py

--- caesar/__init__.py
+++ caesar/__init__.py
@@ -26,7 +26,7 @@ def test_intarg():
     """input of \"python caesar.py 5\" runs correctly"""
     input = "Hello, World!"
     output = "Mjqqt, Btwqi!"
-    check50.run("python3 caesar.py 5").stdin(input[0], prompt=True).stdout(regex(output), output, regex=True).exit()
+    check50.run("python3 caesar.py 5").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
 
 @check50.check(exists)
 def main_function():

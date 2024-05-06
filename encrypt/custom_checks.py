36e12c27ce807443e018f2bbd5684895c15dc2a2
mrpayne207 <54715609+mrpayne207@users.noreply.github.com>
2024-05-06 12:45:19 -0500

    Update custom_checks.py

--- encrypt/custom_checks.py
+++ encrypt/custom_checks.py
@@ -4,13 +4,13 @@ from inspect import getmembers, isfunction
 match sys.argv[1]:
 
     case "main_function":
-        with open("caesar.py") as file:
+        with open("encrypt.py") as file:
 
             # Look for main function definition and avoid importing any libraries
             assert "def main" in "".join(file.read().splitlines())
 
     case "custom_functions":
-        with open("caesar.py") as file:
+        with open("encrypt.py") as file:
             function_counter = 0
             for line in file.readlines():
                 if "def main" in line.strip():

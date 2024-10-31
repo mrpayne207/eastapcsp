e712e0671b3f3c1c0b5a9ebccd8e2bc5b91ddbd2
mrpayne207 <54715609+mrpayne207@users.noreply.github.com>
2024-10-30 21:23:46 -0500

    Update __init__.py

--- calc-ex/__init__.py
+++ calc-ex/__init__.py
@@ -12,7 +12,7 @@ def test():
     """input of x = 4.32 and y = 8.67 yields correct results"""
     input1 = "4.32"
     input2 = "8.67"
-    output = "x + y = 12.99\nx - y = -4.35\nx * y = 37.45\nx / y = 0.50\nx mod y = 4.32\nx ^ y = 323328..57\nx // y = 0.00"
+    output = "x + y = 12.99\nx - y = -4.35\nx * y = 37.45\nx / y = 0.50\nx mod y = 4.32\nx ^ y = 323328.57\nx // y = 0.00"
     check50.run("python3 calc.py").stdin(input1, prompt=True).stdin(input2, prompt=True).stdout(regex(output), output, regex=True).exit()
 
 @check50.check(exists)

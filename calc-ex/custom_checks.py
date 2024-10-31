13b3e7eed678b1394f780eb8e80a9a9c8c6ae32b
mrpayne207 <54715609+mrpayne207@users.noreply.github.com>
2024-10-30 21:21:20 -0500

    Create custom_checks.py

---
+++ calc-ex/custom_checks.py
@@ -0,0 +1,22 @@
+import sys
+from inspect import getmembers, isfunction
+
+match sys.argv[1]:
+
+    case "main_function":
+        with open("calc.py") as file:
+
+            # Look for main function definition and avoid importing any libraries
+            assert "def main" in "".join(file.read().splitlines())
+
+    case "custom_functions":
+        with open("calc.py") as file:
+            function_counter = 0
+            for line in file.readlines():
+                if "def main" in line.strip():
+                    continue
+                elif "def" in line.strip():
+                    function_counter += 1
+            
+            # Ensure there are at least 1 top-level functions other than main
+            assert function_counter >= 1

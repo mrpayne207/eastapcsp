d802b5722725951904a6a13a7492ec6ce209b8a3
mrpayne207 <54715609+mrpayne207@users.noreply.github.com>
2024-10-31 12:39:07 -0500

    Update __init__.py

--- relativity/__init__.py
+++ relativity/__init__.py
@@ -9,14 +9,14 @@ def exists():
 @check50.check(exists)
 def test1():
     """input of 1 yields output of 90000000000000000"""
-    output = check50.run("python3 relativity.py").stdin("1", prompt=False).stdout("90000000000000000").exit()
+    output = check50.run("python3 relativity.py").stdin("1", prompt=True).stdout("90000000000000000").exit()
 
 @check50.check(exists)
 def test14():
     """input of 14 yields output of 1260000000000000000"""
-    check50.run("python3 relativity.py").stdin("14", prompt=False).stdout("1260000000000000000").exit()
+    check50.run("python3 relativity.py").stdin("14", prompt=True).stdout("1260000000000000000").exit()
 
 @check50.check(exists)
 def test50():
     """input of 50 yields output of 4500000000000000000"""
-    check50.run("python3 relativity.py").stdin("50", prompt=False).stdout("4500000000000000000").exit()
+    check50.run("python3 relativity.py").stdin("50", prompt=True).stdout("4500000000000000000").exit()

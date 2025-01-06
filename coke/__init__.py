ffd1579251924c3a63576333d9b3ee5423701f7b
mrpayne207 <54715609+mrpayne207@users.noreply.github.com>
2025-01-06 14:07:28 -0600

    Update __init__.py

--- coke/__init__.py
+++ coke/__init__.py
@@ -25,7 +25,7 @@ def test_10():
 
 
 @check50.check(exists)
-def test_1():
+def test_5():
     """coke accepts 5 cents"""
     input = "5"
     output = "45"
@@ -36,7 +36,7 @@ def test_1():
 def test_invalid():
     """coke rejects invalid amount of cents"""
     input = "30"
-    output = "50"
+    output = "Enter 25 for a quarter, 10 for a dime or 5 for a nickel"
     check50.run("python3 coke.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).kill()
 
 
@@ -44,15 +44,15 @@ def test_invalid():
 def test_multiple():
     """coke accepts continued input"""
     input = "10"
-    output = "30"
+    output = "Remaining amount owed: 30 cents"
     check50.run("python3 coke.py").stdin(input, prompt=True).stdin(input, prompt=True).stdout(regex(output), output, regex=True).kill()
 
 
 @check50.check(exists)
 def test_terminate():
     """coke terminates at 50 cents"""
-    input = "10"
-    output = "0"
+    input = "25"
+    output = "You paid with the following coins:\nquarter, quarter,\nChange owed back to you: 0 cents"
     check50.run("python3 coke.py").stdin(input, prompt=True).stdin(input, prompt=True).stdin(input, prompt=True).stdin(input, prompt=True).stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
 
 
@@ -63,6 +63,30 @@ def test_change():
     output = "10"
     check50.run("python3 coke.py").stdin(input, prompt=True).stdin("10", prompt=True).stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
 
+@check50.check(exists)
+def main_function():
+    """main function exists"""
+    check50.include("custom_checks.py")
+    check50.run("python3 custom_checks.py main_function").exit(0)
+
+
+@check50.check(exists)
+def custom_function():
+    """implemented at least 1 top-level function other than main"""
+    check50.include("custom_checks.py")
+    check50.run("python3 custom_checks.py custom_functions").exit(0)
+
+@check50.check(exists)
+def condition():
+    """implemented at least 1 condition"""
+    check50.include("custom_checks.py")
+    check50.run("python3 custom_checks.py condition").exit(0)
+
+@check50.check(exists)
+def loop():
+    """implemented at least 1 loop"""
+    check50.include("custom_checks.py")
+    check50.run("python3 custom_checks.py loop").exit(0)
 
 def regex(text):
     """match case-sensitively, allowing for characters (not numbers) on either side. Ensure not negative with no dashes"""

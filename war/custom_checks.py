6bc92d43c77b15905ce92c2a0d1f570c8ad98d1b
mrpayne207 <54715609+mrpayne207@users.noreply.github.com>
2025-01-17 15:40:16 -0600

    Create custom_checks.py

---
+++ war/custom_checks.py
@@ -0,0 +1,43 @@
+import sys
+from inspect import getmembers, isfunction
+
+match sys.argv[1]:
+
+    case "main_function":
+        with open("war.py") as file:
+
+            # Look for main function definition and avoid importing any libraries
+            assert "def main" in "".join(file.read().splitlines())
+
+    case "custom_functions":
+        with open("war.py") as file:
+            function_counter = 0
+            for line in file.readlines():
+                if "def main" in line.strip():
+                    continue
+                elif "def" in line.strip():
+                    function_counter += 1
+            
+            # Ensure there are at least 1 top-level functions other than main
+            assert function_counter >= 1
+    
+    case "condition":
+        with open("war.py") as file:
+            condition_counter = 0
+            for line in file.readlines():
+                if ("if" or "elif" or "else") in line.strip():
+                    condition_counter += 1
+            
+            # Ensure there are at least 1 top-level functions other than main
+            assert condition_counter >= 1
+            
+    
+    case "loop":
+        with open("war.py") as file:
+            loop_counter = 0
+            for line in file.readlines():
+                if ("for" or "while") in line.strip():
+                    loop_counter += 1
+           
+            # Ensure there is at least 1 loop in the program
+            assert loop_counter >= 1

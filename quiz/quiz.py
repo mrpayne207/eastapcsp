ece2bea31a874e11abb6b1925bc26536dcce0271
mrpayne207 <54715609+mrpayne207@users.noreply.github.com>
2025-02-24 11:16:18 -0600

    Update quiz.py

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -5,17 +5,17 @@
 import random
 
 def main():
-    #generate random list of unique numbers
+    # validate getting a positive integer of states and capitals to quiz from the user
     # TODO
 
     # get a list of states and their capitals
-    states, capitals = states_and_caps(num)
+    quiz_states, quiz_caps = states_and_caps(num)
 
     # play the quiz game
-    quiz(states, capitals)
+    quiz(quiz_states, quiz_caps)
 
 # define a procedure quiz that takes in a list of states and a list of their capitals, and plays a quiz game
-def quiz(states, capitals):
+def quiz(quiz_states, quiz_caps):
     #initialize score to 0
     # TODO
 
@@ -45,8 +45,10 @@ def states_and_caps(num):
     # list all states and their capitals
     all_states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Hawaii", "Florida", "Georgia", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "North Carolina", "North Dakota", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
     all_capitals = ["Montgomery", "Juneau", "Phoenix", "Little Rock", "Sacramento", "Denver", "Hartford", "Dover", "Honolulu", "Tallahassee", "Atlanta", "Boise", "Springfield", "Indianapolis", "Des Moines", "Topeka", "Frankfort", "Baton Rouge", "Augusta", "Annapolis", "Boston", "Lansing", "St. Paul", "Jackson", "Jefferson City", "Helena", "Lincoln", "Carson City", "Concord", "Trenton", "Santa Fe", "Raleigh", "Bismarck", "Albany", "Columbus", "Oklahoma City", "Salem", "Harrisburg", "Providence", "Columbia", "Pierre", "Nashville", "Austin", "Salt Lake City", "Montpelier", "Richmond", "Olympia", "Charleston", "Madison", "Cheyenne"]
+    
     # set up blank lists for the quiz
-    # TODO
+    quiz_states = []
+    quiz_caps = []
 
     # get the states and their capitals based on their index values
     # TODO
